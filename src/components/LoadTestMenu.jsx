72f86e210ef75227b4abff88444a32e834dbb34b
Levin <Levin.Kerschberger@novatec-gmbh.de>
2023-05-25 10:17:35 +0200

    deleted:    domainstory-schema.proto 	modified:   src/components/LoadTestMenu.jsx 	modified:   src/data/loadtest-specs.json 	modified:   src/utils/createEdges.jsx

--- src/components/LoadTestMenu.jsx
+++ src/components/LoadTestMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useEdges, useOnSelectionChange, useReactFlow } from 'reactflow';
 import * as loadtestSpecs from '../data/loadtest-specs.json';
+import * as mapping from '../data/werkstatt.json';
 
 export default function LoadTestMenu(props) {
 
@@ -9,14 +10,45 @@ export default function LoadTestMenu(props) {
     const responseMeasures = loadtestSpecs.responseMeasures;
     const metrics = loadtestSpecs.metrics;
 
+    let initRQADefiniton = {
+        context: mapping.context,
+        environment: mapping.server_info[0].environment,
+        runtime_quality_analysis: {
+            resilience: [],
+            loadtests: [
+                {
+                    artifact: { object: props.selectedEdge.system, activity: props.selectedEdge.activity },
+                    description: props.selectedEdge.name,
+                    parametrization: props.selectedEdge.parametrization,
+                    response_measure: {},
+                    result_metrics: [],
+                    stimulus: {
+                        load_profile: stimuluses[0].name.toUpperCase().replace(/\s+/g, '_'),
+                        accuracy: 0
+                    }
+
+
+                }
+            ]
+        }
+    }
+
+
+
+
 
     const [selectedActivity, setSelectedActivity] = useState(props.selectedEdge);
     const [stimulus, setStimulus] = useState(stimuluses[0]);
     const [accuracy, setAccuracy] = useState(0);
     const [designParameters, setDesignParameters] = useState(stimuluses[0].designParameters);
+    // Later there could be more than one response measure...
+    const [responseMeasure, setResponseMeasure] = useState([]);
 
 
-    const [includedMetrics, setIncludedMetrics] = useState(["Response Times"]);
+    // state-based RQA-definition
+    const [rqa, setRqa] = useState(initRQADefiniton);
+
+    const [includedMetrics, setIncludedMetrics] = useState(["response_time"]);
 
     const reactFlowInstance = useReactFlow();
 
@@ -35,6 +67,12 @@ export default function LoadTestMenu(props) {
         let relatedEdgesArray = reactFlowInstance.getEdges().filter((edge) => edge.name == e.target.value);
         let unrelatedEdgesArray = reactFlowInstance.getEdges().filter((edge) => edge.name != e.target.value);
 
+        let rqaArtifact;
+
+        // const regexId = /^system_(\w+)$/;
+        // const idMatch = regexId.exec(edgeToService.source);
+        // const serviceId = idMatch ? idMatch[1] : null;
+
         unrelatedEdgesArray.forEach((edge) => {
             edge.selected = false;
         })
@@ -47,43 +85,146 @@ export default function LoadTestMenu(props) {
 
         // add the unselected version od the edge
         reactFlowInstance.setEdges(newEdgesArray);
-
         setSelectedActivity(e.target.value);
+        let rqaCopy = rqa;
+
+
+        // Only works with one loadtest
+        rqaCopy.runtime_quality_analysis.loadtests[0].artifact.object = props.selectedEdge.system;
+        rqaCopy.runtime_quality_analysis.loadtests[0].artifact.activity = props.selectedEdge.activity;
+        rqaCopy.runtime_quality_analysis.loadtests[0].description = props.selectedEdge.name;
+        console.log(rqaCopy.runtime_quality_analysis.loadtests[0]);
+
+        setRqa(rqaCopy);
+
 
     }
 
     const handleStimulusChange = (e) => {
         const newStimulus = stimuluses.find((stimulus) => stimulus.name == e.target.value);
+        let rqaCopy = rqa;
+        rqaCopy.runtime_quality_analysis.loadtests[0].stimulus.load_profile = e.target.value.toUpperCase().replace(/\s+/g, '_');
         console.log(newStimulus);
         setStimulus(newStimulus);
-
         setDesignParameters(newStimulus.designParameters);
+        setRqa(rqaCopy);
     }
 
     const handleAccuracyChange = (e) => {
         setAccuracy(e.target.value);
+        let rqaCopy = rqa;
+        rqaCopy.runtime_quality_analysis.loadtests[0].stimulus.accuracy = e.target.value;
         console.log(accuracy);
     }
 
     const handleMetricsChange = (e) => {
+
         const value = e.target.value;
         const isChecked = e.target.checked;
+        let rqaCopy = rqa;
         if (isChecked) {
             setIncludedMetrics([...includedMetrics, value]);
+            rqaCopy.runtime_quality_analysis.loadtests[0].result_metrics.push(value);
         } else {
             setIncludedMetrics(includedMetrics.filter(item => item !== value));
+            rqaCopy.runtime_quality_analysis.loadtests[0].result_metrics.filter((metric) => metric !== value);
+
         }
+
+    }
+
+    const handleDesignParameterChange = (e) => {
+        const parameter_name = e.target.parentNode.id.toLowerCase().replace(/\s+/g, '_');
+        const value = e.target.value.toUpperCase().replace(/\s+/g, '_');
+        let rqaCopy = rqa;
+        rqaCopy.runtime_quality_analysis.loadtests[0].stimulus[parameter_name] = value;
+        setRqa(rqaCopy);
+        //setStimulusParameters([...stimulusParameters], parameter)
+    }
+
+    const handleResponseMeasureChange = (event) => {
+        const measureName = event.target.parentNode.parentNode.firstChild.innerText;
+        const value = event.target.value.toUpperCase();
+        let rqaCopy = rqa;
+
+        // Check if an object with the measurename already exists in the responseMeasure-State
+        const index = responseMeasure.findIndex((obj) => obj.measureName === measureName);
+
+        let measureNameRqaFormatted = measureName.toLowerCase().replace(/\s+/g, '_');
+
+        if (index === -1) {
+            // If an object with the measurename does not exist, add a new object to the responseMeasure array
+            setResponseMeasure([...responseMeasure, { value, measureName }]);
+            rqaCopy.runtime_quality_analysis.loadtests[0].response_measure[measureNameRqaFormatted] = value;
+            setRqa(rqaCopy);
+
+        } else {
+            // If an object with the measurename already exists, update its value property
+            const updatedResponseMeasure = [...responseMeasure];
+            updatedResponseMeasure[index].value = value;
+            setResponseMeasure(updatedResponseMeasure);
+            rqaCopy.runtime_quality_analysis.loadtests[0].response_measure[measureNameRqaFormatted] = value;
+            setRqa(rqaCopy);
+
+        }
+        console.log(responseMeasure)
 
     }
 
     const submitLoadtest = () => {
 
+        const selectedEdges = reactFlowInstance.getEdges().filter((edge) => edge.selected == true);
+        const regex = /^system_/; // matches strings that start with "system_"
+
+        //only works with one Service in path...
+        const edgeToService = selectedEdges.find((edge) => regex.test(edge.source));
+
+        const regexId = /^system_(\w+)$/;
+        const idMatch = regexId.exec(edgeToService.source);
+        const serviceId = idMatch ? idMatch[1] : null;
+
+        let includedMetricsFormatted = [];
+        includedMetrics.forEach((metric) => {
+            let formattedMetric = metric.toUpperCase().replace(/\s+/g, '_');
+            includedMetricsFormatted.push(formattedMetric);
+        });
+
+        //TODO: Validate, if all the Settings are set.
+
+        //First server_info of the mapping is hardcoded
+        // Only the response_measure "response_time" is used for now. RQA-Definition Schema should be changed in dqTranslator
+        // const rqaDefinition = {
+        //     context: mapping.context,
+        //     environment: mapping.server_info[0].environment,
+        //     runtime_quality_analysis: {
+        //         resilience: [],
+        //         loadtests: [
+        //             {
+        //                 artifact: {
+        //                     object: serviceId,
+        //                     activity: selectedActivity.mappingId
+        //                 },
+        //                 description: selectedActivity.name,
+        //                 parametrization: selectedActivity.parametrization,
+        //                 response_measure: { response_time: responseMeasure[0].value },
+        //                 result_metrics: includedMetricsFormatted
+
+        //             }
+        //         ]
+        //     }
+        // }
+
+        console.log("Current RQA:");
+        console.log(rqa);
     }
 
 
     useEffect(() => {
-        console.log(props.selectedEdge);
         setSelectedActivity(props.selectedEdge);
+        let rqaCopy = rqa;
+        rqaCopy.runtime_quality_analysis.loadtests[0].artifact = { object: props.selectedEdge.system, activity: props.selectedEdge.activity }
+        rqaCopy.runtime_quality_analysis.loadtests[0].description = props.selectedEdge.name
+        setRqa(rqaCopy);
     }, [props.selectedEdge]);
 
     return (
@@ -120,7 +261,7 @@ export default function LoadTestMenu(props) {
                                 <p>{responseMeasure.name}</p>
                                 <div className="button-group">
                                     {responseMeasure.values.map((value) => {
-                                        return <button>{value}</button>
+                                        return <button value={value} onClick={handleResponseMeasureChange}>{value}</button>
                                     })}
                                 </div>
                             </div>
@@ -135,9 +276,9 @@ export default function LoadTestMenu(props) {
                             return (
                                 <div>
                                     <p>{parameter.name}</p>
-                                    <div className="button-group">
+                                    <div id={parameter.name} className="button-group">
                                         {parameter.values.map((value) => {
-                                            return <button>{value}</button>
+                                            return <button value={value} onClick={handleDesignParameterChange}>{value}</button>
                                         })}
                                     </div>
                                 </div>
@@ -152,9 +293,9 @@ export default function LoadTestMenu(props) {
                                     <div>
                                         <p>{parameter.name}</p>
                                         <select name="" id="">
-                                            {parameter.values.map((value) => {
+                                            {parame1ter.values.map((value) => {
                                                 return (
-                                                    <option value="">{value}</option>
+                                                    <option value={value}>{value}</option>
                                                 )
 
                                             })}
