c8fb6a912dd19906c227e6943482f582328d3934
Levin <Levin.Kerschberger@novatec-gmbh.de>
2023-07-05 18:48:01 +0200

    deleted tests

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -84,30 +84,10 @@ export default function Home() {
 
     const queryClient = useQueryClient();
 
-    const postsQuery = useQuery({
-        queryKey: ["post"],
-        queryFn: () => wait(1000).then(() => [...POSTS])
-    })
-
-    const newPost = useMutation({
-        mutationFn: (title) => {
-            return wait(1000).then(() => POSTS.push({ id: crypto.randomUUID(), title }))
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries("post");
-        }
-    })
-
-    if (postsQuery.isLoading) return <h1>Loading...</h1>
-    if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>
-
-
     const { domains } = useLoaderData();
 
     return (
         <div class="flex items-center justify-center h-screen flex-col">
-            {postsQuery.data.map(post => (<div key={post.id}>{post.title}</div>))}
-            <Button className='btn' onClick={() => newPost.mutate("New Post")}>New Post</Button>
             <div class="text-center">
                 <img src={dqLogo} alt="" srcset="" class="mx-auto mb-4 w-1/2" />
                 <p class="font-bold text-2xl">Welcome to dqAnalyzer 1.0!</p>
