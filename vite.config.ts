e75a495ad733bc508bcca9a3ee8b70f11d8ba2f9
Daniel <daniel.flat@wps.de>
2023-09-07 13:36:09 +0200

    df: rebase with master v8

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config();
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({
-    // Add this line
-    include: "**/*.jsx",
-  })],
-  envDir: "./"
-})
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+
+export default ({ mode }) => {
+    const env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+    return defineConfig({
+        plugins: [react()],
+        build: {
+            outDir: 'dist/app',
+        },
+        define: {
+            __VITE_BACKEND_URL__: JSON.stringify(env.VITE_BACKEND_URL)
+        },
+    })
+
+};
